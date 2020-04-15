e785ad56a7e941c8fc2999fa1d7f4cf3690b1bf0
Guillaume La Roque <glaroque@baylibre.com>
2020-04-15 10:04:00 +0200

    configs: sei510: add 512M to system partition
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

--- include/configs/sei510.h
+++ include/configs/sei510.h
@@ -20,8 +20,8 @@
 	"name=boot,size=32M,bootable,uuid=${uuid_gpt_boot};" \
 	"name=recovery,size=32M,uuid=${uuid_gpt_recovery};"	\
 	"name=cache,size=256M,uuid=${uuid_gpt_cache};"	\
-	"name=super,size=1792M,uuid=${uuid_gpt_super};"	\
-	"name=userdata,size=5340M,uuid=${uuid_gpt_userdata};" \
+	"name=super,size=2304M,uuid=${uuid_gpt_super};"	\
+	"name=userdata,size=4828M,uuid=${uuid_gpt_userdata};" \
 	"name=rootfs,size=-,uuid=" ROOT_UUID
 
 
