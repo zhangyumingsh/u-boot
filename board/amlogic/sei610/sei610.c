b30f4d5fdc20bf28147652d6509081c63ac84eab
Guillaume La Roque <glaroque@baylibre.com>
2020-06-29 09:28:14 +0200

    board: sei610: generate unique serial number
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

--- board/amlogic/sei610/sei610.c
+++ board/amlogic/sei610/sei610.c
@@ -15,6 +15,7 @@
 #include <asm/arch/sm.h>
 #include <asm/arch/eth.h>
 #include <asm/arch/mem.h>
+#include <u-boot/crc.h>
 
 #if CONFIG_IS_ENABLED(FASTBOOT_FLASH_CUSTOM) && CONFIG_IS_ENABLED(MMC_WRITE)
 
@@ -176,12 +177,39 @@ bool fastboot_board_erase(const char *cmd, char *response)
 
 int misc_init_r(void)
 {
+	char chip_serial[16];
+	char serial_string[12];
+	u8 serial[6];
+	u32 sid;
+	u16 sid16;
+
 	meson_eth_init(PHY_INTERFACE_MODE_RMII,
 		       MESON_USE_INTERNAL_RMII_PHY);
 
 	meson_generate_serial_ethaddr();
 
-	env_set("serial#", "AMLG12ASEI610");
+	if (!env_get("serial#")) {
+		if (!meson_sm_get_serial(chip_serial, SM_SERIAL_SIZE)) {
+			sid = crc32(0, (unsigned char *)chip_serial, SM_SERIAL_SIZE);
+			sid16 = crc16_ccitt(0, (unsigned char *)chip_serial,	SM_SERIAL_SIZE);
+
+			/* Ensure the NIC specific bytes of the mac are not all 0 */
+			if ((sid & 0xffffff) == 0)
+				sid |= 0x800000;
+
+			/* Non OUI  */
+			serial[0] = ((sid16 >> 8) & 0xfc) | 0x02;
+			serial[1] = (sid16 >>  0) & 0xff;
+			serial[2] = (sid >> 24) & 0xff;
+			serial[3] = (sid >> 16) & 0xff;
+			serial[4] = (sid >>  8) & 0xff;
+			serial[5] = (sid >>  0) & 0xff;
+			sprintf(serial_string, "%02X%02X%02X%02X%02X%02X", serial[0], serial[1], serial[2],serial[3], serial[4], serial[5]);
+			env_set("serial#", serial_string);
+
+		} else
+			return -EINVAL;
+	}
 
 	return 0;
 }
