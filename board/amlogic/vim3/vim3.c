de44e287b65865fa4e3fad0bef1773a4740f5706
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2020-11-13 14:26:00 +0100

    board: amlogic: vim3: generate serial# on boot
    This is based on the sei610 commit b6b5c0dcaf0e ("board: sei610:
    generate unique serial number")
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- board/amlogic/vim3/vim3.c
+++ board/amlogic/vim3/vim3.c
@@ -11,6 +11,7 @@
 #include <net.h>
 #include <asm/io.h>
 #include <asm/arch/eth.h>
+#include <asm/arch/sm.h>
 #include <i2c.h>
 #include "khadas-mcu.h"
 
@@ -131,7 +132,38 @@ int meson_ft_board_setup(void *blob, struct bd_info *bd)
 
 int misc_init_r(void)
 {
+	char chip_serial[16];
+	char serial_string[12];
+	u8 serial[6];
+	u32 sid;
+	u16 sid16;
+
 	meson_eth_init(PHY_INTERFACE_MODE_RGMII, 0);
+
+	meson_generate_serial_ethaddr();
+
+	if (!env_get("serial#")) {
+		if (!meson_sm_get_serial(chip_serial, SM_SERIAL_SIZE)) {
+			sid = crc32(0, (unsigned char *)chip_serial, SM_SERIAL_SIZE);
+			sid16 = crc16_ccitt(0, (unsigned char *)chip_serial,	SM_SERIAL_SIZE);
+
+			/* Ensure the NIC specific bytes of the mac are not all 0 */
+			if ((sid & 0xffffff) == 0)
+				sid |= 0x800000;
+
+			/* Non OUI  */
+			serial[0] = ((sid16 >> 8) & 0xfc) | 0x02;
+			serial[1] = (sid16 >>  0) & 0xff;
+			serial[2] = (sid >> 24) & 0xff;
+			serial[3] = (sid >> 16) & 0xff;
+			serial[4] = (sid >>  8) & 0xff;
+			serial[5] = (sid >>  0) & 0xff;
+			sprintf(serial_string, "%02X%02X%02X%02X%02X%02X", serial[0], serial[1], serial[2],serial[3], serial[4], serial[5]);
+			env_set("serial#", serial_string);
+
+		} else
+			return -EINVAL;
+	}
 
 	return 0;
 }
