f20c7479a992c80c08f137536d738df59c5a0252
Yuntian Zhang <yt@radxa.com>
2022-06-29 16:34:26 +0800

    Revert "mmc: meson_gx_mmc: Add MMC_MESON_GX_HS52_MODE config"
    This reverts commit 789ec6d58e2bc60c7b676c4eedb683e34abec5ad.

--- drivers/mmc/meson_gx_mmc.c
+++ drivers/mmc/meson_gx_mmc.c
@@ -277,11 +277,7 @@ static int meson_mmc_probe(struct udevice *dev)
 	cfg->voltages = MMC_VDD_33_34 | MMC_VDD_32_33 |
 			MMC_VDD_31_32 | MMC_VDD_165_195;
 	cfg->host_caps = MMC_MODE_8BIT | MMC_MODE_4BIT |
-#ifdef CONFIG_MMC_MESON_GX_HS52_MODE
 			MMC_MODE_HS_52MHz | MMC_MODE_HS;
-#else
-			MMC_MODE_HS;
-#endif
 	cfg->f_min = DIV_ROUND_UP(SD_EMMC_CLKSRC_24M, CLK_MAX_DIV);
 	cfg->f_max = 100000000; /* 100 MHz */
 	cfg->b_max = 511; /* max 512 - 1 blocks */
